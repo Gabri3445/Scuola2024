99fb3fdc3403e68fef620ce30762303c33363072
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-15 19:43:14 +0200

    implemented api function

--- TPSIT/WeatherForecast/src/pages/forecast/index.tsx
+++ TPSIT/WeatherForecast/src/pages/forecast/index.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from "react";
+import { useMemo, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { LocationData, getForecastTemperature } from "../../weatherApi/api";
 
 const forecast = () => {
     function useQuery() {
@@ -7,6 +8,15 @@ const forecast = () => {
 
         return useMemo(() => new URLSearchParams(search), [search]);
     }
+
+    const [forecast, setForecast] = useState<LocationData>()
+
+    useEffect(() => {
+        async function getForecast() {
+            setForecast(await getForecastTemperature(latitude, longitude))
+        }
+        getForecast()
+    }, [])
 
     const latitude = parseInt(useQuery().get("lat") ?? "0")
     const longitude = parseInt(useQuery().get("lon") ?? "0")
