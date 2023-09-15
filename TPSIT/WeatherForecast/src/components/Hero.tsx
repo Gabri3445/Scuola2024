5bd64ba2503e79c8bca4105260cb7120b057548f
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-15 23:37:33 +0200

    made the text responsive

--- TPSIT/WeatherForecast/src/components/Hero.tsx
+++ TPSIT/WeatherForecast/src/components/Hero.tsx
@@ -1,9 +1,9 @@
-const Hero = ({children}) => {
+const Hero = ({children}: any) => {
     return (
         <>
-            <div className="w-full h-screen flex justify-center bg-no-repeat flex-col bg-cover bg-[url('https://images.pexels.com/photos/2114014/pexels-photo-2114014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
-                <h1 className="text-white font-bold text-7xl text-center">Weather Forecast</h1>
-                <h2 className="subtitle text-white font-semibold text-6xl text-center">Get the best weather forecast for your location</h2>
+            <div className="w-full h-screen flex justify-center bg-no-repeat flex-col bg-cover bg-[url('https://images.pexels.com/photos/557782/pexels-photo-557782.jpeg')]">
+                <h1 className="text-white font-bold text-xl md:text-7xl text-center">Weather Forecast</h1>
+                <h2 className="subtitle text-white font-semibold text-lg md:text-6xl text-center">Get the best weather forecast for your location</h2>
                 {children}
             </div>
         </>
