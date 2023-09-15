d0f9c1fc4d0a5a65fbc2b6ca40dd4f82024e039b
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-15 09:16:05 +0200

    Added file based routing

--- TPSIT/WeatherForecast/src/main.tsx
+++ TPSIT/WeatherForecast/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+/*
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+)*/
+import { createRoot } from 'react-dom/client'
+import { Routes } from '@generouted/react-router'
+
+const app = document.getElementById("root") as HTMLElement
+createRoot(app).render(<Routes/>)
---
+++ TPSIT/WeatherForecast/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Hero from "../components/Hero"
+import PosForm from "../components/PosForm"
+
+const index = () => {
+    return (
+        <>
+            <Hero>
+                <PosForm />
+            </Hero>
+        </>
+    )
+}
+
+export default index 
