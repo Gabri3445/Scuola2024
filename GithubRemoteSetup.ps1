96323ff8a5a634f0ff95427e32b1094c88fdd825
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-15 19:56:12 +0200

    Script for adding a second remote

---
+++ GithubRemoteSetup.ps1
@@ -0,0 +1,6 @@
+git remote add github "https://github.com/Gabri3445/Scuola2024.git"
+
+# Set the default remote and branch for the first remote (origin)
+git push --set-upstream origin main
+
+Write-Output "use 'git push' to push to the first remote (origin) and 'git push github main' to push to the second remote."
