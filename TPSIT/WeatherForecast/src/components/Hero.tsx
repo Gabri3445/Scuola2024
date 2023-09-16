1c5271ce4ccf22f4935631653ee6da2d5d9dec40
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-16 08:50:44 +0200

    More mobile changes

--- TPSIT/WeatherForecast/src/components/Hero.tsx
+++ TPSIT/WeatherForecast/src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = ({children}: any) => {
     return (
         <>
-            <div className="w-full h-screen flex justify-center bg-no-repeat flex-col bg-cover bg-[url('https://images.pexels.com/photos/557782/pexels-photo-557782.jpeg')]">
+            <div className="w-screen h-screen flex pt-12 md:pt-0 md:justify-center bg-no-repeat flex-col bg-cover bg-[url('https://images.pexels.com/photos/557782/pexels-photo-557782.jpeg')]">
                 <h1 className="text-white font-bold text-xl md:text-7xl text-center">Weather Forecast</h1>
                 <h2 className="subtitle text-white font-semibold text-lg md:text-6xl text-center">Get the best weather forecast for your location</h2>
                 {children}
