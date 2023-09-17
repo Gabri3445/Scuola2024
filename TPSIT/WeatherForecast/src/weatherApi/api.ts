14c9a95e63ce94eae1abb8fc231235f6d46632c8
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-18 00:10:50 +0200

    changed api for more data

--- TPSIT/WeatherForecast/src/weatherApi/api.ts
+++ TPSIT/WeatherForecast/src/weatherApi/api.ts
@@ -1,7 +1,8 @@
-export const getForecastTemperature = async (latitude : number, longitude: number): Promise<LocationData> => {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current_weather=true`;
+
+export const getForecastTemperature = async (latitude : number, longitude: number): Promise<WeatherData> => {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,surface_pressure,visibility,windspeed_10m,uv_index,is_day&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max&current_weather=true&timezone=auto`;
     const response = await fetch(url);
-    const data: LocationData = await response.json();
+    const data: WeatherData = await response.json();
     return data;
 }
 
@@ -11,20 +12,52 @@ interface CurrentWeather {
     winddirection: number;
     weathercode: number;
     is_day: number;
-    time: string; // ISO 8601 format
-}
-
-interface HourlyUnit {
-    time: string; // ISO 8601 format
-    temperature_2m: string; 
-}
-
-interface HourlyData {
-    time: string[]; 
-    temperature_2m: number[]; 
-}
-
-interface LocationData {
+    time: string;
+  }
+  
+  interface HourlyUnits {
+    time: string;
+    temperature_2m: string;
+    apparent_temperature: string;
+    precipitation_probability: string;
+    weathercode: string;
+    surface_pressure: string;
+    visibility: string;
+    windspeed_10m: string;
+    uv_index: string;
+    is_day: string;
+  }
+  
+  interface HourlyData {
+    time: string[];
+    temperature_2m: number[];
+    apparent_temperature: number[];
+    precipitation_probability: number[];
+    weathercode: number[];
+    surface_pressure: number[];
+    visibility: number[];
+    windspeed_10m: number[];
+    uv_index: number[];
+    is_day: number[];
+  }
+  
+  interface DailyUnits {
+    time: string;
+    weathercode: string;
+    temperature_2m_max: string;
+    temperature_2m_min: string;
+    precipitation_probability_max: string;
+  }
+  
+  interface DailyData {
+    time: string[];
+    weathercode: number[];
+    temperature_2m_max: number[];
+    temperature_2m_min: number[];
+    precipitation_probability_max: number[];
+  }
+  
+  interface WeatherData {
     latitude: number;
     longitude: number;
     generationtime_ms: number;
@@ -33,10 +66,13 @@ interface LocationData {
     timezone_abbreviation: string;
     elevation: number;
     current_weather: CurrentWeather;
-    hourly_units: HourlyUnit;
+    hourly_units: HourlyUnits;
     hourly: HourlyData;
-}
+    daily_units: DailyUnits;
+    daily: DailyData;
+  }
+  
 
-export type {LocationData};
+export type {WeatherData};
 
-//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true
+//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,surface_pressure,visibility,windspeed_10m,uv_index,is_day&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_probability_max&current_weather=true&timezone=auto
