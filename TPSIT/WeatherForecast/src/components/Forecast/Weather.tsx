fc6f64bd7eef984663b9266df1bc617b1e1aa46d
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-18 01:29:45 +0200

    Added current weather

--- TPSIT/WeatherForecast/src/components/Forecast/Weather.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/Weather.tsx
@@ -13,19 +13,28 @@ const Weather = ({latitude, longitude}: Props) => {
 
     useEffect(() => {
         async function getForecast() {
-            setForecast(await getForecastTemperature(latitude, longitude))
+                setForecast(await getForecastTemperature(latitude, longitude))
         }
         getForecast()
     }, [])
-
-
-    return (
-        <div className="flex flex-col text-white justify-center items-center h-full">
-            <div className="h-5/6 w-5/6 bg-black/75 rounded-2xl">
-                <CurrentWeather temperature={24} wmo={"0"}></CurrentWeather>
+    
+    if(forecast) {
+        return (
+            <div className="flex flex-col text-white md:justify-center items-center h-full">
+                <div className="h-5/6 w-5/6 bg-black/75 rounded-2xl">
+                    <CurrentWeather temperature={forecast.current_weather.temperature} 
+                    wmo={"" + forecast.current_weather.weathercode} 
+                    apparentTemperature={forecast.hourly.apparent_temperature[0]} 
+                    wind={forecast.current_weather.windspeed}
+                    humidity={forecast.hourly.relativehumidity_2m[0]}
+                    uv={forecast.hourly.uv_index[0]}
+                    visibility={forecast.hourly.visibility[0]}
+                    pressure={forecast.hourly.surface_pressure[0]}
+                    ></CurrentWeather>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Weather;
