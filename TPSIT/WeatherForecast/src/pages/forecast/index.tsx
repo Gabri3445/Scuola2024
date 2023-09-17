3d8a70c76454918ba439a3b85445c17066808aff
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-18 00:11:19 +0200

    forecast index page

--- TPSIT/WeatherForecast/src/pages/forecast/index.tsx
+++ TPSIT/WeatherForecast/src/pages/forecast/index.tsx
@@ -1,29 +1,23 @@
-import { useMemo, useEffect, useState } from "react";
+import { useMemo } from "react";
 import { useLocation } from "react-router-dom";
-import { LocationData, getForecastTemperature } from "../../weatherApi/api";
+import Weather from "../../components/Forecast/Weather";
 
 const forecast = () => {
-    function useQuery() {
+   function useQuery() {
         const { search } = useLocation();
 
         return useMemo(() => new URLSearchParams(search), [search]);
     }
-
-    const [forecast, setForecast] = useState<LocationData>()
-
-    useEffect(() => {
-        async function getForecast() {
-            setForecast(await getForecastTemperature(latitude, longitude))
-        }
-        getForecast()
-    }, [])
 
     const latitude = parseInt(useQuery().get("lat") ?? "0")
     const longitude = parseInt(useQuery().get("lon") ?? "0")
 
     return (
         <>
-        
+        <div className="w-full h-screen flex md:pt-0 bg-no-repeat flex-col bg-cover bg-[url('https://images.pexels.com/photos/557782/pexels-photo-557782.jpeg')]">
+            <h1 className="text-white font-bold text-xl md:text-7xl text-center">Weather Forecast</h1>
+            <Weather latitude={latitude} longitude={longitude} />
+        </div>
         </>
     )
 }
