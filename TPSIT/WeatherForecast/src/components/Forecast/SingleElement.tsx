cabb49e549a24cfe05683af046fcf43c54941b64
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-18 01:27:27 +0200

    single element for the current weather

---
+++ TPSIT/WeatherForecast/src/components/Forecast/SingleElement.tsx
@@ -0,0 +1,15 @@
+interface Props {
+    label: string;
+    value: string;
+}
+
+const SingleElement = ({label, value}: Props) => {
+    return (
+        <div className="text-3xl flex flex-col items-start ml-2 mr-2 border-white border rounded-md p-5 3xl:border-none 3xl:p-0">
+            <div className="text-center w-full">{label}</div>
+            <div className="text-center w-full flex-1 flex items-center justify-center text-5xl">{value}</div>
+        </div>
+    )
+}
+
+export default SingleElement;
