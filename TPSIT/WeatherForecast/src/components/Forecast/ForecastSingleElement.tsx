830eb4f2b991a463d2a5059743f7709ea0dcb4fb
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-18 16:38:39 +0200

    Forecast single element component

---
+++ TPSIT/WeatherForecast/src/components/Forecast/ForecastSingleElement.tsx
@@ -0,0 +1,11 @@
+const ForecastSingleElement = () => {
+    return (
+        <>
+        <div className="border-2 h-1/5 border-white rounded-lg mt-4 pb-2 flex flex-row justify-center items-center">
+                  Day number - Day name
+        </div>
+        </>
+    )
+}
+
+export default ForecastSingleElement;
