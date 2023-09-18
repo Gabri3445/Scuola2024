9737eddf4688cab35d784fc16d05c7b8b2957876
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-18 16:21:46 +0200

    forecast component

---
+++ TPSIT/WeatherForecast/src/components/Forecast/Forecast.tsx
@@ -0,0 +1,14 @@
+const Forecast = () => {
+    return (
+        <>
+        <div className="text-white text-5xl px-5 flex-1 flex flex-col">
+                    <div className="mb-2">Forecast:</div>
+                    <div className="text-9xl flex border-2 border-white rounded-lg content-center pb-4 pl-2 justify-center flex-wrap h-full flex-1 overflow-auto mb-4">
+                        
+                    </div>
+                </div>
+        </>
+    )
+}
+
+export default Forecast;
