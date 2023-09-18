47831fc99e62ae09abb9608ab99e661ff00d0060
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-18 16:21:06 +0200

    more responsive changes

--- TPSIT/WeatherForecast/src/components/Forecast/SingleElement.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/SingleElement.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const SingleElement = ({label, value}: Props) => {
     return (
-        <div className="text-3xl flex flex-col items-start ml-2 mr-2 border-white border rounded-md p-5 3xl:border-none 3xl:p-0">
+        <div className="text-3xl flex flex-col items-start ml-2 mr-2 border-white border rounded-md p-5 m-1 3xl:m-0 3xl:border-none 3xl:p-0">
             <div className="text-center w-full">{label}</div>
             <div className="text-center w-full flex-1 flex items-center justify-center text-5xl">{value}</div>
         </div>
