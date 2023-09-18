9cb0e90b8e6f75d3a8121185251c2ebd67eb14ab
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-18 08:49:11 +0200

    display the correct hour fix

--- TPSIT/WeatherForecast/src/components/Forecast/Weather.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/Weather.tsx
@@ -10,12 +10,15 @@ interface Props {
 const Weather = ({latitude, longitude}: Props) => {
 
     const [forecast, setForecast] = useState<WeatherData>()
+    const [hour, setHour] = useState(0);
 
     useEffect(() => {
         async function getForecast() {
                 setForecast(await getForecastTemperature(latitude, longitude))
         }
         getForecast()
+        const date = new Date();
+        setHour(date.getHours())
     }, [])
     
     if(forecast) {
@@ -24,12 +27,12 @@ const Weather = ({latitude, longitude}: Props) => {
                 <div className="h-5/6 w-5/6 bg-black/75 rounded-2xl">
                     <CurrentWeather temperature={forecast.current_weather.temperature} 
                     wmo={"" + forecast.current_weather.weathercode} 
-                    apparentTemperature={forecast.hourly.apparent_temperature[0]} 
+                    apparentTemperature={forecast.hourly.apparent_temperature[hour]} 
                     wind={forecast.current_weather.windspeed}
-                    humidity={forecast.hourly.relativehumidity_2m[0]}
-                    uv={forecast.hourly.uv_index[0]}
-                    visibility={forecast.hourly.visibility[0]}
-                    pressure={forecast.hourly.surface_pressure[0]}
+                    humidity={forecast.hourly.relativehumidity_2m[hour]}
+                    uv={forecast.hourly.uv_index[hour]}
+                    visibility={forecast.hourly.visibility[hour]}
+                    pressure={forecast.hourly.surface_pressure[hour]}
                     ></CurrentWeather>
                 </div>
             </div>
