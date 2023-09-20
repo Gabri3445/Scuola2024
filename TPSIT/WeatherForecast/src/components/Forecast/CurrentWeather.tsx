b336c37008231ca8849d99549ef4dbb69f8436a3
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-20 10:03:48 +0200

    moved decode wmo function

--- TPSIT/WeatherForecast/src/components/Forecast/CurrentWeather.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/CurrentWeather.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import SingleElement from "./SingleElement";
+import decodeWmo from "../../utils/wmo";
 
 interface Props {
     temperature: number;
@@ -18,55 +19,6 @@ const CurrentWeather = (props: Props) => {
     useEffect(() => {
         setWmo(decodeWmo(props.wmo))
     }, [])
-
-
-    const decodeWmo = (wmo: string): string => {
-        switch (wmo) {
-            case "0":
-                return "Clear sky"
-            case "1":
-            case "2":
-            case "3":
-                return "Cloudy"
-            case "45":
-            case "48":
-                return "Fog"
-            case "51":
-            case "53":
-            case "55":
-                return "Drizzle"
-            case "56":
-            case "57":
-                return "Freezing Drizzle"
-            case "61":
-            case "63":
-            case "65":
-                return "Rain"
-            case "66":
-            case "67":
-                return "Freezing Rain"
-            case "71":
-            case "73":
-            case "75":
-                return "Snow fall"
-            case "77":
-                return "Snow grains"
-            case "80":
-            case "81":
-            case "82":
-                return "Rain showers"
-            case "85":
-            case "86":
-                return "Snow showers"
-            case "95":
-                return "Thunderstorm"
-            case "96":
-            case "99":
-                return "Thunderstorm with heavy hail"
-        }
-        return "Error"
-        
-    }
 
 
         return (
