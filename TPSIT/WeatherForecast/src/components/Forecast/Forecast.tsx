aafa8ceb9c32ef59935b2454626a7d5d0d137927
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-20 12:22:54 +0200

    ForecastSingleElement mapped and selected index

--- TPSIT/WeatherForecast/src/components/Forecast/Forecast.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/Forecast.tsx
@@ -10,6 +10,7 @@ interface Props {
 const Forecast = (props: Props) => {
     const [HourlyData, setHourlyData] = useState<HourlyData | null>();
     const [DailyData, setDailyData] = useState<DailyData | null>();
+    const [index, setIndex] = useState(-1)
     const forecastLength = 7; //Change if needed
 
     useEffect(() => {
@@ -32,6 +33,10 @@ const Forecast = (props: Props) => {
             //TODO: do the same for hourly data but for 24 hours
         }
       }, [DailyData]);
+
+      const handleClick = (index: number) => {
+        setIndex(index)
+      }
       
 
     if (DailyData != undefined && HourlyData != undefined) {
@@ -39,8 +44,10 @@ const Forecast = (props: Props) => {
             <>
                 <div className="text-white text-5xl px-5 overflow:flex flex-col h-3/4 hidden">
                     <div className="mb-2">Forecast:</div>
-                    <div className="flex flex-col border-2 text-4xl border-white rounded-lg pb-4 pl-2 flex-grow overflow-auto mb-4">
-                        <ForecastSingleElement date={new Date(DailyData.time[0])} temperature={[DailyData.temperature_2m_min[1], DailyData.temperature_2m_max[1]]} precipitation={DailyData.precipitation_probability_max[0]}></ForecastSingleElement>
+                    <div className="flex flex-col border-2 text-4xl h-full border-white rounded-lg pb-4 pl-2 flex-grow overflow-auto mb-4">
+                        {DailyData.time.map((_, index) => (
+                            <ForecastSingleElement key={index} onClick={() => handleClick(index)} date={new Date(DailyData.time[index])} precipitation={DailyData.precipitation_probability_max[index]} temperature={[DailyData.temperature_2m_min[index], DailyData.temperature_2m_max[index]]}></ForecastSingleElement>
+                        ))}
                     </div>
                 </div>
             </>
