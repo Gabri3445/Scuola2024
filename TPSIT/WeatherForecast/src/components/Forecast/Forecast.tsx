d988d92d4145f2dc82f6917108e014c9264e3ece
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-20 13:47:53 +0200

    Added wmo prop and variable day forecast

--- TPSIT/WeatherForecast/src/components/Forecast/Forecast.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/Forecast.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { DailyData, HourlyData } from "../../weatherApi/api";
 import ForecastSingleElement from "./ForecastSingleElement";
+import decodeWmo from "../../utils/wmo";
 
 interface Props {
     HourlyData : string;
@@ -10,7 +11,7 @@ interface Props {
 const Forecast = (props: Props) => {
     const [HourlyData, setHourlyData] = useState<HourlyData | null>();
     const [DailyData, setDailyData] = useState<DailyData | null>();
-    const [index, setIndex] = useState(-1)
+    const [_, setIndex] = useState(-1)
     const forecastLength = 7; //Change if needed
 
     useEffect(() => {
@@ -43,10 +44,10 @@ const Forecast = (props: Props) => {
         return (
             <>
                 <div className="text-white text-5xl px-5 overflow:flex flex-col h-3/4 hidden">
-                    <div className="mb-2">Forecast:</div>
+                    <div className="mb-2">{forecastLength} Day Forecast:</div>
                     <div className="flex flex-col border-2 text-4xl h-full border-white rounded-lg pb-4 pl-2 flex-grow overflow-auto mb-4">
                         {DailyData.time.map((_, index) => (
-                            <ForecastSingleElement key={index} onClick={() => handleClick(index)} date={new Date(DailyData.time[index])} precipitation={DailyData.precipitation_probability_max[index]} temperature={[DailyData.temperature_2m_min[index], DailyData.temperature_2m_max[index]]}></ForecastSingleElement>
+                            <ForecastSingleElement wmo={decodeWmo(DailyData.weathercode[index] + "")} key={index} onClick={() => handleClick(index)} date={new Date(DailyData.time[index])} precipitation={DailyData.precipitation_probability_max[index]} temperature={[DailyData.temperature_2m_min[index], DailyData.temperature_2m_max[index]]}></ForecastSingleElement>
                         ))}
                     </div>
                 </div>
