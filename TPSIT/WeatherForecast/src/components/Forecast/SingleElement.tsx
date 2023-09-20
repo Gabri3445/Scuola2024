b2ef59c286da6e3adb61af6169406f5864255441
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-20 10:18:59 +0200

    added conditional border on single element

--- TPSIT/WeatherForecast/src/components/Forecast/SingleElement.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/SingleElement.tsx
@@ -1,15 +1,23 @@
 interface Props {
     label: string;
     value: string;
+    border? : boolean
 }
 
-const SingleElement = ({label, value}: Props) => {
-    return (
-        <div className="text-3xl flex flex-col items-start ml-2 mr-2 border-white border rounded-md p-5 m-1 3xl:m-0 3xl:border-none 3xl:p-0">
-            <div className="text-center w-full">{label}</div>
-            <div className="text-center w-full flex-1 flex items-center justify-center text-5xl">{value}</div>
-        </div>
-    )
-}
+import { useMemo } from 'react';
+
+const SingleElement = ({ label, value, border }: Props) => {
+  const containerClass = useMemo(() => {
+    return `text-3xl flex flex-col items-start ml-2 mr-2 p-5 m-1 3xl:m-0 3xl:border-none ${border ? " border-white border rounded-md" : ""}`;
+  }, [border]);
+
+  return (
+    <div className={containerClass}>
+      <div className="text-center w-full">{label}</div>
+      <div className="text-center w-full flex-1 flex items-center justify-center text-5xl">{value}</div>
+    </div>
+  );
+};
+
 
 export default SingleElement;
