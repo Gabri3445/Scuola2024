7b989f8139ec6e9b4b934fa108460b7f4862c960
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-22 20:44:26 +0200

    added summary to modal

--- TPSIT/WeatherForecast/src/components/Forecast/ForecastModal.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/ForecastModal.tsx
@@ -1,11 +1,23 @@
+import ForecastSingleElement from "./ForecastSingleElement";
 
-const ForecastModal = () => {
+interface Props {
+    date: Date;
+    temperature: number[];
+    precipitation: number;
+    wmo: string
+}
+
+const ForecastModal = ({ date, temperature, precipitation, wmo }: Props) => {
     return (
-        
-      <div className="w-3/4 h-3/4 bg-gray-900 border-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-white rounded-2xl ml-auto mr-auto">
-        
-      </div>
-    
+
+        <div className="w-3/4 h-3/4 text-white text-4xl bg-gray-900 border-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-white rounded-2xl ml-auto mr-auto">
+            <div className="w-full">
+                <div className="w-3/4 mx-auto">
+                    <ForecastSingleElement date={date} temperature={temperature} precipitation={precipitation} wmo={wmo}></ForecastSingleElement>
+                </div>
+            </div>
+        </div>
+
     );
 
 }
