ca682f46aa31d1b6db28c4359abe9b33521cd499
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-22 22:23:19 +0200

    preparations for the graph

--- TPSIT/WeatherForecast/src/components/Forecast/ForecastModal.tsx
+++ TPSIT/WeatherForecast/src/components/Forecast/ForecastModal.tsx
@@ -10,11 +10,14 @@ interface Props {
 const ForecastModal = ({ date, temperature, precipitation, wmo }: Props) => {
     return (
 
-        <div className="w-3/4 h-3/4 text-white text-4xl bg-gray-900 border-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-white rounded-2xl ml-auto mr-auto">
+        <div className="w-3/4 h-3/4 text-white flex flex-col text-4xl bg-gray-900 border-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-white rounded-2xl ml-auto mr-auto">
             <div className="w-full">
                 <div className="w-3/4 mx-auto">
                     <ForecastSingleElement date={date} temperature={temperature} precipitation={precipitation} wmo={wmo}></ForecastSingleElement>
                 </div>
+            </div>
+            <div className="bg-red w-full flex-1">
+                Graph goes here
             </div>
         </div>
 
