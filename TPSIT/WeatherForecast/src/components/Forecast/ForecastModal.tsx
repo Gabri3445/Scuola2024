0bbabcc8dfd33d43ad3df1f393c5dcbda7c84a0c
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-22 20:31:42 +0200

    modal component

---
+++ TPSIT/WeatherForecast/src/components/Forecast/ForecastModal.tsx
@@ -0,0 +1,13 @@
+
+const ForecastModal = () => {
+    return (
+        
+      <div className="w-3/4 h-3/4 bg-gray-900 border-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-white rounded-2xl ml-auto mr-auto">
+        
+      </div>
+    
+    );
+
+}
+
+export default ForecastModal;
