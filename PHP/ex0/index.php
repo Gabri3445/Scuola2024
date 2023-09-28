7b4beb5ea5ecacf5551d113a9276285bd4063ed9
Gabriele Lopez <gabriele.ope@gmail.com>
2023-09-28 12:24:56 +0200

    Php exercise 1

---
+++ PHP/ex0/index.php
@@ -0,0 +1,12 @@
+<?php
+  $nome = "Gabriele";
+  $cognome = "Lopez";
+  $citta = "Bari";
+  $provincia = "BA";
+
+  
+  echo "Nome: <b>" . $nome . "</b> <br> \n";
+  echo "Cognome: " . $cognome . "<br> \n";
+  echo "Città: " . $citta . "<br> \n";
+  echo "Provincia: " . $provincia;
+?>
---
+++ PHP/ex0bis/index.php
@@ -0,0 +1,12 @@
+<?php 
+$number = 200;
+$number2 = 300;
+$number3 = 50;
+
+echo "The value of A = ".$number."<br>";
+echo "The value of B = ".$number2."<br>";
+echo "The value of C = ".$number3."<br><br>";
+
+echo "The sum of A and B = ".($number + $number2)."<br>";
+echo "The division of B and C = ".($number2 / $number3)."<br>";
+?>
