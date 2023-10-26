9e84c5199fa3e5ce27fdf05dd6fa47ace12e259e
Gabriele Lopez <gabriele.ope@gmail.com>
2023-10-26 09:16:32 +0200

    Ex 2

---
+++ TPSIT/PHP/Exercises3/ex2/index.php
@@ -0,0 +1,34 @@
+<form method="post" action=<?php echo $_SERVER["PHP_SELF"] ?>>
+<div>
+    <label for="login">Login:</label>
+    <input type="text" id="login" name="login"/>
+</div>
+<div>
+    <label for="pwd">Password:</label>
+    <input type="text" id="pwd" name="pwd"/>
+</div>
+<input type="submit">
+</form>
+
+<?php
+
+function checkInput($log, $pass) {
+    $login = "admin";
+    $password = "nimda";
+    if ($log == $login && $pass == $password) {
+        return "Login and password are ok";
+    }
+    else if ($log != $login && $pass == $password) {
+        return "Login is wrong";
+    }
+    else if ($log == $login && $pass != $password) {
+        return "Password is wrong";
+    }
+    else if ($log != $login && $pass != $password) {
+        return "Login and password are wrong";
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo checkInput($_POST["login"],$_POST["pwd"]);
+}
