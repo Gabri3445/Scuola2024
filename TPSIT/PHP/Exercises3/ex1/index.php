bc2ac6e26638671157efb367e401c55050ea4fb6
Gabriele Lopez <gabriele.ope@gmail.com>
2023-10-26 08:49:45 +0200

    ex1

---
+++ TPSIT/PHP/Exercises3/ex1/index.php
@@ -0,0 +1,44 @@
+<form method="post" action=<?php echo $_SERVER["PHP_SELF"] ?> >
+<label for="cents">Insert the amount in cents</label>
+<input type="number" id="cents" name="cents">
+<input type="submit">
+</form>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $amount = $_POST["cents"];
+
+        if (is_numeric($amount) || $amount > 0) {
+            $amount = (int)$amount;
+
+            echo "Amount: " . number_format($amount / 100, 2) . " euro<br>";
+            echo "Coins necessary:<br>";
+
+            $euro2 = intdiv($amount, 200);
+            $amount %= 200;
+            $euro1 = intdiv($amount, 100);
+            $amount %= 100;
+            $cent50 = intdiv($amount, 50);
+            $amount %= 50;
+            $cent20 = intdiv($amount, 20);
+            $amount %= 20;
+            $cent10 = intdiv($amount, 10);
+            $amount %= 10;
+            $cent5 = intdiv($amount, 5);
+            $amount %= 5;
+            $cent2 = intdiv($amount, 2);
+            $amount %= 2;
+            $cent1 = $amount;
+
+            if ($euro2 > 0) echo "$euro2 x 2 euro<br>";
+            if ($euro1 > 0) echo "$euro1 x 1 euro<br>";
+            if ($cent50 > 0) echo "$cent50 x 50 cent<br>";
+            if ($cent20 > 0) echo "$cent20 x 20 cent<br>";
+            if ($cent10 > 0) echo "$cent10 x 10 cent<br>";
+            if ($cent5 > 0) echo "$cent5 x 5 cent<br>";
+            if ($cent2 > 0) echo "$cent2 x 2 cent<br>";
+            if ($cent1 > 0) echo "$cent1 x 1 cent<br>";
+        } else {
+            echo "Enter a valid number of cents";
+        }
+    }
