1f240797cc65ae757f26632ca1e7431f1ddd43b3
Gabriele Lopez <gabriele.ope@gmail.com>
2023-12-05 23:13:07 +0100

    ex1

---
+++ TPSIT/PHP/Exercises6/ex1/index.php
@@ -0,0 +1,43 @@
+<form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post" >
+    Enter 5 numbers
+    <?php
+    for ($i = 0; $i < 5; $i++) {
+        echo "
+        <div>
+        <label for=$i>$i</label>
+        <input required id=$i name=$i type='number'>
+        </div>
+        ";
+    }
+    ?>
+    <div>
+        <label for='number'>Enter a number</label>
+        <input type='number' name='number' id='number'>
+    </div>
+    <input type="submit">
+</form>
+
+
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"]) {
+    for ($i = 0; $i < 5; $i++) {
+        $array[$i] = $_POST[$i];
+    }
+    echo "The number is present ". countNumberOccurrences($array, $_POST["number"]) ." times";
+}
+
+
+function countNumberOccurrences($array, $number) {
+    $count = 0;
+    $arrayLength = count($array);
+
+    for ($i = 0; $i < $arrayLength; $i++) {
+        if ($array[$i] == $number) {
+            $count++;
+        }
+    }
+
+    return $count;
+}
