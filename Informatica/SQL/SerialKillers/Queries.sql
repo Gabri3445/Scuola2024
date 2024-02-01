0acdcc9dc696af48d2b7b92937bf30f246a6151b
Gabriele Lopez <gabriele.ope@gmail.com>
2024-02-01 20:09:37 +0100

    formatting

--- Informatica/SQL/SerialKillers/Queries.sql
+++ Informatica/SQL/SerialKillers/Queries.sql
@@ -1,175 +1,301 @@
 use crimine;
 
 -- 1
-select * from serialkiller;
+select *
+from serialkiller;
 
 -- 2
-select count(*) from serialkiller;
+select count(*)
+from serialkiller;
 
 -- 3
-select * from serialkiller order by Vittimeaccertate desc limit 1;
+select *
+from serialkiller
+order by Vittimeaccertate desc
+limit 1;
 
 -- 4
-select * from serialkiller order by PossibiliVittime desc limit 1;
+select *
+from serialkiller
+order by PossibiliVittime desc
+limit 1;
 
 -- 5
-select * from serialkiller where AnnoInizio>=1980 and AnnoInizio<=1995 and AnnoFine<=1995;
+select *
+from serialkiller
+where AnnoInizio >= 1980
+  and AnnoInizio <= 1995
+  and AnnoFine <= 1995;
 
 -- 6
-select * from serialkiller where Note is not null;
+select *
+from serialkiller
+where Note is not null;
 
 -- 7
-select * from serialkiller where Nazione like "%Italy%" order by Vittimeaccertate desc;
+select *
+from serialkiller
+where Nazione like "%Italy%"
+order by Vittimeaccertate desc;
 
 -- 8
-select * from serialkiller where Vittimeaccertate>=10 and Vittimeaccertate<=20;
+select *
+from serialkiller
+where Vittimeaccertate >= 10
+  and Vittimeaccertate <= 20;
 
 -- 9
-select * from serialkiller where AnnoFine<=1990;
+select *
+from serialkiller
+where AnnoFine <= 1990;
 
 -- 10
-select * from serialkiller order by Vittimeaccertate limit 1;
+select *
+from serialkiller
+order by Vittimeaccertate
+limit 1;
 
 -- 11
-select * from serialkiller where Nazione like "%United States%" order by Vittimeaccertate limit 1;
+select *
+from serialkiller
+where Nazione like "%United States%"
+order by Vittimeaccertate
+limit 1;
 
 -- 12
-select distinct Nazione from serialkiller;
+select distinct Nazione
+from serialkiller;
 
 -- 13
-select * from serialkiller where AnnoFine=0 and AnnoInizio=0;
+select *
+from serialkiller
+where AnnoFine = 0
+  and AnnoInizio = 0;
 
 -- 14
-select * from serialkiller where AnnoFine=AnnoInizio+10;
+select *
+from serialkiller
+where AnnoFine = AnnoInizio + 10;
 
 -- 15
-select avg(Vittimeaccertate) from serialkiller where Nazione like "%Russia%";
+select avg(Vittimeaccertate)
+from serialkiller
+where Nazione like "%Russia%";
 
 -- 16
-select * from serialkiller where Nome like "_B%";
+select *
+from serialkiller
+where Nome like "_B%";
 
 -- 17
-select * from serialkiller where PossibiliVittime>20;
+select *
+from serialkiller
+where PossibiliVittime > 20;
 
 -- 18
-select * from serialkiller where Nome="Richard Ramirez";
+select *
+from serialkiller
+where Nome = "Richard Ramirez";
 
 -- 19
-select * from serialkiller where PossibiliVittime!=0;
+select *
+from serialkiller
+where PossibiliVittime != 0;
 
 -- 20
-select * from serialkiller where Note like "%Known%" or Note like "%Nicknamed%";
+select *
+from serialkiller
+where Note like "%Known%"
+   or Note like "%Nicknamed%";
 
 -- 21
-select count(*) from serialkiller where AnnoFine=0;
+select count(*)
+from serialkiller
+where AnnoFine = 0;
 
 -- 22
-select count(*) from serialkiller where AnnoFine!=0;
+select count(*)
+from serialkiller
+where AnnoFine != 0;
 
 -- 23
-select * from serialkiller where Vittimeaccertate=15 and Nazione like "%United States%";
+select *
+from serialkiller
+where Vittimeaccertate = 15
+  and Nazione like "%United States%";
 
 -- 24
-select * from serialkiller where AnnoFine<2000 and AnnoFine!=0;
+select *
+from serialkiller
+where AnnoFine < 2000
+  and AnnoFine != 0;
 
 -- 25
-select * from serialkiller where Note like "%The Butcher of Rostov%";
+select *
+from serialkiller
+where Note like "%The Butcher of Rostov%";
 
 -- 26
-select * from serialkiller where Vittimeaccertate>5 and Nazione like "%India%";
+select *
+from serialkiller
+where Vittimeaccertate > 5
+  and Nazione like "%India%";
 
 -- 27
-select * from serialkiller where Nazione like "%,%";
+select *
+from serialkiller
+where Nazione like "%,%";
 
 -- 28
-select * from serialkiller where AnnoInizio=0 and AnnoFine=0;
+select *
+from serialkiller
+where AnnoInizio = 0
+  and AnnoFine = 0;
 
 -- 29
-select * from serialkiller where Vittimeaccertate>10 or PossibiliVittime>25;
+select *
+from serialkiller
+where Vittimeaccertate > 10
+   or PossibiliVittime > 25;
 
 -- 30
-select AnnoInizio, AnnoFine, Vittimeaccertate from serialkiller;
+select AnnoInizio, AnnoFine, Vittimeaccertate
+from serialkiller;
 
 -- 31
-select sum(Vittimeaccertate) from serialkiller;
+select sum(Vittimeaccertate)
+from serialkiller;
 
 -- 32
-select Nome, Note from serialkiller;
+select Nome, Note
+from serialkiller;
 
 -- 33
-select avg(Vittimeaccertate) from serialkiller;
+select avg(Vittimeaccertate)
+from serialkiller;
 
 -- 34
-select Nome, AnnoInizio, AnnoFine from serialkiller where Vittimeaccertate>10;
+select Nome, AnnoInizio, AnnoFine
+from serialkiller
+where Vittimeaccertate > 10;
 
 -- 35
-select sum(Vittimeaccertate), sum(PossibiliVittime) from serialkiller;
+select sum(Vittimeaccertate), sum(PossibiliVittime)
+from serialkiller;
 
 -- 36
-select count(*) from serialkiller where AnnoFine<1980;
+select count(*)
+from serialkiller
+where AnnoFine < 1980;
 
 -- 37
-select Nome, Nazione, Note from serialkiller;
+select Nome, Nazione, Note
+from serialkiller;
 
 -- 38
-select * from serialkiller order by Vittimeaccertate desc limit 1;
+select *
+from serialkiller
+order by Vittimeaccertate desc
+limit 1;
 
 -- 39
-select Nome, PossibiliVittime from serialkiller where PossibiliVittime>5;
+select Nome, PossibiliVittime
+from serialkiller
+where PossibiliVittime > 5;
 
 -- 40
-select count(*) from serialkiller where AnnoFine<2000;
+select count(*)
+from serialkiller
+where AnnoFine < 2000;
 
 -- 41
-select Nome, AnnoInizio from serialkiller where AnnoFine=0;
+select Nome, AnnoInizio
+from serialkiller
+where AnnoFine = 0;
 
 -- 42
-select * from serialkiller order by Vittimeaccertate limit 1;
+select *
+from serialkiller
+order by Vittimeaccertate
+limit 1;
 
 -- 43
-select Nome, Nazione from serialkiller where AnnoInizio>1979 and AnnoFine<2001;
+select Nome, Nazione
+from serialkiller
+where AnnoInizio > 1979
+  and AnnoFine < 2001;
 
 -- 44
-select sum(Vittimeaccertate), Nazione from serialkiller group by Nazione;
+select sum(Vittimeaccertate), Nazione
+from serialkiller
+group by Nazione;
 
 -- 45
-select Nome, AnnoInizio, AnnoFine from serialkiller where Vittimeaccertate>20;
+select Nome, AnnoInizio, AnnoFine
+from serialkiller
+where Vittimeaccertate > 20;
 
 -- 46
-select * from serialkiller where Note like "%Psychopath%";
+select *
+from serialkiller
+where Note like "%Psychopath%";
 
 -- 47
-select Nome, Vittimeaccertate+PossibiliVittime from serialkiller;
+select Nome, Vittimeaccertate + PossibiliVittime
+from serialkiller;
 
 -- 48
-select * from serialkiller order by Vittimeaccertate+PossibiliVittime desc limit 1;
+select *
+from serialkiller
+order by Vittimeaccertate + PossibiliVittime desc
+limit 1;
 
 -- 49
-select count(*), Nazione from serialkiller group by Nazione;
+select count(*), Nazione
+from serialkiller
+group by Nazione;
 
 -- 50
-select AVG(AnnoFine - AnnoInizio) from serialkiller;
+select AVG(AnnoFine - AnnoInizio)
+from serialkiller;
 
 -- 51
-select Nome, Nazione from serialkiller where AnnoFine!=0;
+select Nome, Nazione
+from serialkiller
+where AnnoFine != 0;
 
 -- 52
-select count(*) from serialkiller where Vittimeaccertate>3;
+select count(*)
+from serialkiller
+where Vittimeaccertate > 3;
 
 -- 53
-select Nome, Note from serialkiller where AnnoFine=0;
+select Nome, Note
+from serialkiller
+where AnnoFine = 0;
 
 -- 54
-select * from serialkiller where Vittimeaccertate>=18;
+select *
+from serialkiller
+where Vittimeaccertate >= 18;
 
 -- 55
-select Nome, Nazione from serialkiller;
+select Nome, Nazione
+from serialkiller;
 
 -- 56
-select Nome, Nazione from serialkiller where Vittimeaccertate>=18;
+select Nome, Nazione
+from serialkiller
+where Vittimeaccertate >= 18;
 
 -- 57
-select * from serialkiller where AnnoInizio>=1980 and AnnoFine <=1990;
+select *
+from serialkiller
+where AnnoInizio >= 1980
+  and AnnoFine <= 1990;
 
 -- 58
-select * from serialkiller where AnnoInizio < 1990 and (AnnoFine is not null and AnnoFine between 1990 and 1999);
+select *
+from serialkiller
+where AnnoInizio < 1990
+  and (AnnoFine is not null and AnnoFine between 1990 and 1999);
--- Informatica/SQL/VideoGame/Queries.sql
+++ Informatica/SQL/VideoGame/Queries.sql
@@ -1,69 +1,143 @@
 use videogiochi;
 
-select * from videogame;
+select *
+from videogame;
 
-select distinct nome from videogame;
+select distinct nome
+from videogame;
 
-select * from videogame order by user_review desc;
+select *
+from videogame
+order by user_review desc;
 
-select * from videogame order by meta_score;
+select *
+from videogame
+order by meta_score;
 
-select count(*) from videogame;
+select count(*)
+from videogame;
 
-select nome, piattaforma, data_rilascio from videogame;
+select nome, piattaforma, data_rilascio
+from videogame;
 
-select nome, data_rilascio from videogame where piattaforma=" PlayStation 4";
+select nome, data_rilascio
+from videogame
+where piattaforma = " PlayStation 4";
 
-select nome, data_rilascio from videogame where piattaforma like "%Playstation%";
+select nome, data_rilascio
+from videogame
+where piattaforma like "%Playstation%";
 
-select * from videogame where piattaforma=" Switch" or piattaforma=" Wii" or piattaforma=" Wii U";
+select *
+from videogame
+where piattaforma = " Switch"
+   or piattaforma = " Wii"
+   or piattaforma = " Wii U";
 
-select * from videogame where piattaforma like "%PlayStation%" or piattaforma=" DS";
+select *
+from videogame
+where piattaforma like "%PlayStation%"
+   or piattaforma = " DS";
 
-select distinct nome from videogame order by nome;
+select distinct nome
+from videogame
+order by nome;
 
-select * from videogame where piattaforma=" Nintendo 64" and user_review>70;
+select *
+from videogame
+where piattaforma = " Nintendo 64"
+  and user_review > 70;
 
-select count(*) from videogame where piattaforma=" PC";
+select count(*)
+from videogame
+where piattaforma = " PC";
 
-select avg(meta_score - user_review) from videogame;
+select avg(meta_score - user_review)
+from videogame;
 
-select nome, piattaforma, data_rilascio from videogame where nome like "%Super Mario%";
+select nome, piattaforma, data_rilascio
+from videogame
+where nome like "%Super Mario%";
 
-select * from videogame where data_rilascio="2014-10-18" or data_rilascio="2021-10-15";
+select *
+from videogame
+where data_rilascio = "2014-10-18"
+   or data_rilascio = "2021-10-15";
 
-select * from videogame where nome like "M_R%";
+select *
+from videogame
+where nome like "M_R%";
 
-select * from videogame where nome like "%Marvel%";
+select *
+from videogame
+where nome like "%Marvel%";
 
-select sum(user_review) from videogame where piattaforma=" 3DS";
+select sum(user_review)
+from videogame
+where piattaforma = " 3DS";
 
-select * from videogame where year(data_rilascio)<2024-18;
+select *
+from videogame
+where year(data_rilascio) < 2024 - 18;
 
-select avg(user_review) from videogame where nome like "%Mario Kart%";
+select avg(user_review)
+from videogame
+where nome like "%Mario Kart%";
 
-select id from videogame where nome="Mortal Kombat X";
+select id
+from videogame
+where nome = "Mortal Kombat X";
 
-select count(*) from videogame where nome like "%Zelda%";
+select count(*)
+from videogame
+where nome like "%Zelda%";
 
-select distinct piattaforma from videogame;
+select distinct piattaforma
+from videogame;
 
-select count(*) from videogame group by data_rilascio order by count(*) desc;
+select count(*)
+from videogame
+group by data_rilascio
+order by count(*) desc;
 
-select count(*) from videogame group by piattaforma;
+select count(*)
+from videogame
+group by piattaforma;
 
-select piattaforma from videogame group by piattaforma order by count(*) limit 1;
+select piattaforma
+from videogame
+group by piattaforma
+order by count(*)
+limit 1;
 
-select * from videogame order by data_rilascio limit 1;
+select *
+from videogame
+order by data_rilascio
+limit 1;
 
-select * from videogame where month(data_rilascio)=2;
+select *
+from videogame
+where month(data_rilascio) = 2;
 
-select * from videogame where day(data_rilascio)=15;
+select *
+from videogame
+where day(data_rilascio) = 15;
 
-select * from videogame where month(data_rilascio)=2 and (day(data_rilascio)=15 or day(data_rilascio)=20);
+select *
+from videogame
+where month(data_rilascio) = 2
+  and (day(data_rilascio) = 15 or day(data_rilascio) = 20);
 
-select * from videogame where dayofweek(data_rilascio)=3;
+select *
+from videogame
+where dayofweek(data_rilascio) = 3;
 
-select * from videogame where year(data_rilascio)=2005 or year(data_rilascio)=2006;
+select *
+from videogame
+where year(data_rilascio) = 2005
+   or year(data_rilascio) = 2006;
 
-select * from videogame order by user_review desc limit 5;
+select *
+from videogame
+order by user_review desc
+limit 5;
